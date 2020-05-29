11ed86a843041723ed39ace35be0759298373fdf
Harikrishnan Narasiman <Harikrish.narasiman@gmail.com>
2020-05-29 15:18:26 -0400

    updatE

--- functions/index.js
+++ functions/index.js
@@ -7,31 +7,41 @@ const admin = require('firebase-admin');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
-const serviceAccount = require("/Users/harikrish/Downloads/EduCast-8a1fc04ab889.json");
+// const serviceAccount = require("/Users/harikrish/Downloads/EduCast-8a1fc04ab889.json");
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://educast-4e2e3.firebaseio.com'
-});
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: 'https://educast-4e2e3.firebaseio.com'
+// });
+
+admin.initializeApp({});
 
 const db = admin.firestore();
 
-exports.getStudents = functions.https.onRequest((request, response) => {
-    let response_data = [];
-    db.collection('student').get()
-    .then((snapshot) => {
-        response.send(snapshot.docs.map(doc => doc.data()));
-    })
-})
+// exports.getStudents = functions.https.onRequest((request, response) => {
+//     let response_data = [];
+//     db.collection('student').get()
+//     .then((snapshot) => {
+//         response.send(snapshot.docs.map(doc => doc.data()));
+//     })
+// })
 
 
 
 exports.aggregateSeatsFilled = functions.firestore.document('enrollment/{enrollmentId}').onCreate((change, context) => {
     enrollmentId = context.params.enrollmentId;
-    enrollmentData = db.collection('enrollment').doc(enrollmentId).get()
+    schedule = db.collection('schedules').doc('hffbbRqk8v7ixtaTOH10').get()
     .then(doc => {
         if (!doc.exists){
-            console.log("new enrollment written!" + doc.scheduleid);
+            return null;
+        } else {
+              return doc.ref.update({
+                seatsAvailable : 3
+            });    
         }
     })
+    .catch(err => {
+        console.log('Error', err);
+    });
+    // })
 });
